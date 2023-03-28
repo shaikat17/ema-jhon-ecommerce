b37457cdefbdaa4e87b6f0f9e1b9d9040c272747
shaikat17 <shaikatpal56@gmail.com>
2023-03-28 13:12:49 +0600

    local storage save and retrive with cart calculation is now working

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -14,7 +14,7 @@ const Cart = ({cart}) => {
         // }
         // product.quantity = product.quantity || 1;
 
-        totalPrice = totalPrice + product.price;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
         quantity = quantity + product.quantity;
     }
@@ -25,7 +25,7 @@ const Cart = ({cart}) => {
   return (
     <div className='cart'>
     <h4>Order Summary</h4>
-        <p>Selected Items: {cart.length}</p>
+        <p>Selected Items: {quantity}</p>
         <p>Total Price: ${totalPrice}</p>
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -45,8 +45,23 @@ const Shop = () => {
       }, [products])
 
       const handleAddCart = (product) => {
-        setCart(prev => [...prev, product])
-        // console.log(cart)
+        // cart.push(product); '
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart, then set quantity = 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart= [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
+        setCart(newCart);
         addToDb(product.id)
       }
 
