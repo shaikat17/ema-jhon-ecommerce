304f1acd43efd88538778e0fbe274ee3119183e1
shaikat17 <shaikatpal56@gmail.com>
2023-03-28 11:13:22 +0600

    cart calculation system added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -37,7 +38,7 @@ const Shop = () => {
 
         </div>
         <div className="cart-container">
-            <h2>cart</h2>
+        <Cart cart={cart} />
         </div>
     </div>
   )
