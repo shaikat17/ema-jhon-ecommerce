64a67f1f99390fe7c508e06d38ce955658887611
shaikat17 <shaikatpal56@gmail.com>
2023-04-06 11:15:53 +0600

    working on clear cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import './Cart.css'
 
 
@@ -30,6 +32,7 @@ const Cart = ({cart}) => {
             <p>Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
             <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+            <button className='btn-clear-cart'>Clear Cart <FontAwesomeIcon icon={faTrashAlt} /></button>
     </div>
   )
 }
