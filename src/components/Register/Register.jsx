9c80259c434d08c3eafca9f72d2cb3df0a27d581
shaikat17 <shaikatpal56@gmail.com>
2023-04-19 23:36:04 +0600

    working on firebase registration and login system

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,21 +3,38 @@ import './Register.css'
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+const formHandler = (e) => {
+e.preventDefault()
+
+// console.log('hello')
+const form = e.target
+const userName = form.uname.value
+const email = form.email.value
+const password = form.psw.value
+const passwordRepeat = form.pswRepeat.value
+
+console.log({userName, email, password, passwordRepeat})
+}
+
     return (
-    <form>
+    <form onSubmit={formHandler}>
             <div class="container">
             <h1>Register</h1>
             <p>Please fill in this form to create an account.</p>
             <hr />
 
+            <label for="text"><b>Username</b></label>
+            <input type="text" placeholder="Enter an username" name="uname" id="uname" required />
+
             <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" id="email" required />
+            <input type="email" placeholder="Enter Email" name="email" id="email" required />
 
             <label for="psw"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
 
             <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
+            <input type="password" placeholder="Repeat Password" name="pswRepeat" id="psw-repeat" required />
             <hr />
             <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
